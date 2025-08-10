6b188f845698eb8704216ca1cdb9697378bcf058
jimbostephens <jimbostephens@gmail.com>
2025-08-10 07:54:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,6 @@ function getNextQuestion() {
     currentQuestionIndex = randomIndex;
 
     questionEl.textContent = questions[currentQuestionIndex].question;
-console.log("Current answer:", questions[currentQuestionIndex].answer;
     answerEl.textContent = questions[currentQuestionIndex].answer;
     answerEl.classList.add('hidden'); // Hide the answer initially
 }
