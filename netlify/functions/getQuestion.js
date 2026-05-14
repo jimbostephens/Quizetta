1d1644bc342604a71a9586611a43fa2822b57ff5
jimbostephens <jimbostephens@gmail.com>
2026-05-14 08:42:32 +0100

    eliminate repeats

--- netlify/functions/getQuestion.js
+++ netlify/functions/getQuestion.js
@@ -7,6 +7,32 @@ exports.handler = async (event, context) => {
     // This points to the root of your project where quizetta.db lives
     const dbPath = path.resolve(__dirname, '../../quizetta.db');
 
+// Inside your exports.handler
+const exclude = event.queryStringParameters.exclude;
+
+// If we have IDs to skip, we use "NOT IN", otherwise we just grab any random row
+const query = exclude 
+    ? `SELECT * FROM questions WHERE id NOT IN (${exclude}) ORDER BY RANDOM() LIMIT 1` 
+    : `SELECT * FROM questions ORDER BY RANDOM() LIMIT 1`;
+
+try {
+    const question = await db.get(query);
+    
+    // If the database is "empty" because we've excluded EVERYTHING, 
+    // we should return a message or a fresh random question.
+    if (!question && exclude) {
+        const resetQuestion = await db.get(`SELECT * FROM questions ORDER BY RANDOM() LIMIT 1`);
+        return { statusCode: 200, body: JSON.stringify(resetQuestion) };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(question),
+    };
+} catch (error) {
+    return { statusCode: 500, body: error.toString() };
+}
+
     const db = await open({
       filename: dbPath,
       driver: sqlite3.Database
