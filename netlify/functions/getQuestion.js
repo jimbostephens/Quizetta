1ee38a0d07bea88df0b26db810ac127d071cdf8f
jimbostephens <jimbostephens@gmail.com>
2026-05-15 01:24:20 +0100

    new new Gemini

--- netlify/functions/getQuestion.js
+++ netlify/functions/getQuestion.js
@@ -10,20 +10,21 @@ exports.handler = async (event) => {
 
         db = await open({ filename: dbPath, driver: sqlite3.Database });
 
-        let query = 'SELECT * FROM questions';
-        
-        // Only add the filter if we actually have IDs to exclude
+        // Select rowid explicitly since it's not a standard column
+        let query = 'SELECT rowid, * FROM questions';
+        let params = [];
+
         if (excludeIds && /^[0-9,]+$/.test(excludeIds)) {
-            query += ` WHERE id NOT IN (${excludeIds})`;
+            query += ` WHERE rowid NOT IN (${excludeIds})`;
         }
-        
+
         query += ' ORDER BY RANDOM() LIMIT 1';
 
         let question = await db.get(query);
 
-        // Fallback: If for some reason we run out of questions, just grab any random one
+        // Fallback: if we've excluded too many or something goes wrong
         if (!question) {
-            question = await db.get('SELECT * FROM questions ORDER BY RANDOM() LIMIT 1');
+            question = await db.get('SELECT rowid, * FROM questions ORDER BY RANDOM() LIMIT 1');
         }
 
         await db.close();
